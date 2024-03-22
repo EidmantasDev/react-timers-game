20c0a98dd55401a0a420c7e72a44238f04828eb1
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-22 10:52:28 +0200

    Improved ResultModal component To use useImperativeHandle hook to pass open function to TimerChallange For future proofing the component

--- src/components/TimerChallenge/TimerChallenge.jsx
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -10,7 +10,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -24,7 +24,6 @@ export default function TimerChallenge({ title, targetTime }) {
       <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
       <section className='challenge'>
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
         <p className='challenge-time'>
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
